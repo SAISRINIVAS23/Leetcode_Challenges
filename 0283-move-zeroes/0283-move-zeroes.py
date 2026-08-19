17a7c3c94649ff17ac9997b4a8314335c64fdc77
SAISRINIVAS23 <153480528+SAISRINIVAS23@users.noreply.github.com>
2026-08-19 21:53:42 +0530

    Time: 9 ms (18.76%), Space: 20.4 MB (89.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,15 +1,11 @@
 class Solution:
     def moveZeroes(self, nums):
-        pos=0
-        n=len(nums)
-        for i in range(n):
-            if nums[i]!=0:
-                nums[pos]=nums[i]
-                pos+=1
-        while pos < n:
-            nums[pos] = 0
-            pos += 1
+        j = 0 # Pointer to place the next non-zero element
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # Swap current element with the element at index j 
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1 # Move j to the next index for placing non-zero
 
         return nums
-
      
